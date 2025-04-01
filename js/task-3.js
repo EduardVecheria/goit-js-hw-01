3fadfa3102a807a6059f30c42e26e647b3e041ab
Edward <vecheryaedik@gmail.com>
2025-04-01 17:12:08 +0300

    fixed end

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,9 @@
 function getElementWidth(content, padding, border) {
-
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding)*2;
-  const borderWidth = parseFloat(border)*2;
-
-  return contentWidth + paddingWidth + borderWidth;
+    content = Number.parseFloat(content);
+    padding = Number.parseFloat(padding) * 2;
+    border = Number.parseFloat(border) * 2;
+    return content + border + padding;
 }
-
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
